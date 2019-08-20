b80591da6e01a44aa75b69c7bc9f7e0f0b316e02
Nazhor <dbernad4220@gmail.com>
2019-08-20 23:13:30 +0200

    Setup

---
+++ nazhor.js
@@ -0,0 +1,86 @@
+// function navleft() {
+//     console.log("left");
+// }
+
+// function navright() {
+//     console.log("right");
+//     $("#face1").velocity({ width: 50 }, "easeInSine");
+//     $("#face2").velocity({ width: 50 }, "easeInSine");
+// }
+// $("#nav-right").click(function() {
+//     console.log("right");
+//     alert( "Handler for .click() called." );
+// });
+
+
+
+$(document).ready(function(){
+    var pages = [ "#page0", "#page1" ];
+    var currentPage = 0;
+    var navToPage = 0;
+    var isPlaying = false;
+    var animDuration = 750;
+    var direction = {
+        Right: 0,
+        Left: 1
+    };
+
+    $("#nav-right").click(function(){
+        navigate(direction.Right);
+    });
+
+    $("#nav-left").click(function(){
+        navigate(direction.Left);
+    });
+
+    function navigate( currentDire ) {
+
+        if (!isPlaying){
+            var formWith = $(window).width();
+            var destinyX = 0;
+
+            if (currentPage == 0){
+                navToPage = 1;
+            }else {
+                navToPage = 0;
+            }
+
+            if (currentDire == direction.Right) {
+                $(pages[navToPage]).css("left", -formWith);
+                destinyX = formWith;
+            }else {
+                $(pages[navToPage]).css("left", formWith);
+                destinyX = -formWith;
+            }
+
+            $(pages[navToPage]).show();
+
+            isPlaying = true;
+            $(pages[navToPage]).animate(
+                {
+                    left: 0
+                },
+                {
+                    duration: animDuration
+                }
+            );
+            $(pages[currentPage]).animate(
+                {
+                    left: destinyX
+                },
+                {
+                    duration: animDuration,
+                    complete: function() { hide_element() }
+                }
+            );
+
+            function hide_element() {
+                $(pages[currentPage]).hide();
+                currentPage = navToPage;
+                isPlaying = false;
+            };
+        }
+    };
+
+
+});
